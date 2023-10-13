feef9624837cfad7e88efc25e9b6d41b5897c64b
Sanghoon Lee <gcccompil3r@gmail.com>
2023-10-13 12:12:14 +0900

    feat: [이상훈] Soft Reset을 통한 기존 작업을 유지한 상태의 안전한 복원

--- src/main/java/com/example/demo/diceGame/leeyonggeun/LeeYongGeunController.java
+++ src/main/java/com/example/demo/diceGame/leeyonggeun/LeeYongGeunController.java
@@ -10,4 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/leeyonggeun")
 public class LeeYongGeunController {
 
-    @GetMapping("/test")
+//    @GetMapping("/test")
+}
